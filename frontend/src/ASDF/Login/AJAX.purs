70df8fa95e64bdd8676c7f198a110f7e05890739
rightfold <rightfold@gmail.com>
2018-05-13 00:32:34 +0200

    Add AJAX-based login use case implementation

---
+++ frontend/src/ASDF/Login/AJAX.purs
@@ -0,0 +1,56 @@
+module ASDF.Login.AJAX
+    ( interpret
+    ) where
+
+import Prelude
+
+import ASDF.Authenticate (Token (..))
+import ASDF.Login (EmailAddress, Login (..), Password)
+import Control.Monad.Aff.Class (class MonadAff, liftAff)
+import Control.Monad.Eff.Exception (Error, error)
+import Control.Monad.Error.Class (class MonadError, throwError)
+import Control.MonadZero (guard)
+import Data.Argonaut.Core (Json, jsonEmptyObject)
+import Data.Argonaut.Decode (class DecodeJson, (.?), decodeJson)
+import Data.Argonaut.Encode (class EncodeJson, (~>), (:=), encodeJson)
+import Data.Either (either)
+import Data.Maybe (Maybe)
+import Data.Newtype (unwrap)
+import Network.HTTP.Affjax (AJAX)
+
+import Network.HTTP.Affjax as Affjax
+
+baseURL :: String
+baseURL = "http://localhost:8080"
+
+interpret
+    :: forall eff m
+     . MonadAff (ajax :: AJAX | eff) m
+    => Login ~> m
+interpret (Login next emailAddress password) = liftAff $ do
+    let request = encodeJson $ Request emailAddress password
+    {response} <- Affjax.post (baseURL <> "/login") request
+    Response token <- decodeJsonM response
+    pure <<< next $ token
+
+data Request = Request EmailAddress Password
+newtype Response = Response (Maybe Token)
+
+instance encodeJsonRequest :: EncodeJson Request where
+    encodeJson (Request emailAddress password) =
+        "email_address" := unwrap emailAddress ~>
+        "password"      := unwrap password     ~>
+        jsonEmptyObject
+
+instance decodeJsonResponse :: DecodeJson Response where
+    decodeJson json = do
+        output <- decodeJson json >>= (_ .? "output")
+        pure <<< Response $ Token output <$ guard (output /= "")
+
+decodeJsonM
+    :: forall m a
+     . MonadError Error m
+    => DecodeJson a
+    => Json
+    -> m a
+decodeJsonM = either (throwError <<< error) pure <<< decodeJson
--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -4,9 +4,9 @@ module Main
 
 import Prelude
 
+import Control.Monad.Aff.Class (class MonadAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Free (foldFree)
 import Control.Monad.Rec.Class (class MonadRec)
 import Data.Functor.Coproduct (coproduct)
@@ -16,14 +16,15 @@ import DOM (DOM)
 import Halogen.Aff (HalogenEffects, awaitBody, runHalogenAff)
 import Halogen.Component (hoist)
 import Halogen.VDom.Driver (runUI)
+import Network.HTTP.Affjax (AJAX)
 
 import ASDF.Authenticate.WebStorage as Authenticate.WebStorage
-import ASDF.Login.Dummy as Login.Dummy
+import ASDF.Login.AJAX as Login.AJAX
 import ASDF.Login.UI as Login.UI
 import DOM.HTML (window) as DOM
 import DOM.HTML.Window (localStorage) as DOM
 
-main :: forall eff. Eff (HalogenEffects (console :: CONSOLE | eff)) Unit
+main :: forall eff. Eff (HalogenEffects (ajax :: AJAX | eff)) Unit
 main = runHalogenAff $ do
     interpret <- interpreter
     body <- awaitBody
@@ -32,12 +33,12 @@ main = runHalogenAff $ do
 interpreter
     :: forall eff m eff' m'
      . MonadEff (dom :: DOM | eff) m
-    => MonadEff (console :: CONSOLE, dom :: DOM | eff') m'
+    => MonadAff (ajax :: AJAX, dom :: DOM | eff') m'
     => MonadRec m'
     => m (Login.UI.Monad ~>> m')
 interpreter = liftEff $ do
     window <- DOM.window
     storage <- DOM.localStorage window
     pure $ NT (foldFree (coproduct (Authenticate.WebStorage.interpret storage)
-                        (coproduct (Login.Dummy.interpret)
+                        (coproduct Login.AJAX.interpret
                         (absurd <<< unwrap))))
