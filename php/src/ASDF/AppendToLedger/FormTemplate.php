c95b1908760da5086c95cf12c84650a168380029
rightfold <rightfold@gmail.com>
2018-05-19 15:40:09 +0200

    Add append to ledger form

--- php/src/ASDF/AppendToLedger/FormTemplate.php
+++ php/src/ASDF/AppendToLedger/FormTemplate.php
@@ -14,7 +14,19 @@ final class FormTemplate {
     {
         MasterTemplate::renderHeader('Append to ledger');
         ?>
-            LOL
+            <form action="/append-to-ledger?group=<?php echo \htmlentities($group); ?>"
+                  method="post">
+                <select name="type">
+                    <option>Debt</option>
+                    <option>Payment</option>
+                </select>
+                <textarea name="comment">
+                </textarea>
+                <input type="text" name="debitor">
+                <input type="text" name="creditor">
+                <input type="number" name="amount" step="0.01">
+                <button type="submit">Submit</button>
+            </form>
         <?php
         MasterTemplate::renderFooter();
     }
